b26ecde69d25a918c909bdc0b7fd5afc57ad34b1
Alfreda <alfredadownie@gmail.com>
2016-01-25 17:16:52 -0500

    Added suggestions provided from review which included adding functionality to close active database connections before dropping the tournament table, adding code to maintain table integrity accross related tables as well as making sure the winner and loser ids could not be set to the same values.

--- tournament.sql
+++ tournament.sql
@@ -1,7 +1,13 @@
 -- This file provide the database definitions for tournament database
 
 
--- Create new Datebase.  If the datbase already exists, remove it before
+-- Kill active connections before making changes to tournament database.
+SELECT pg_terminate_backend(pg_stat_activity.pid)
+FROM pg_stat_activity
+WHERE pg_stat_activity.datname = 'tournament'
+  AND pid <> pg_backend_pid();
+
+-- Create new Database.  If the database already exists, remove it before
 -- creating a new one.
 DROP DATABASE IF EXISTS tournament;
 CREATE DATABASE tournament;
@@ -13,13 +19,17 @@ CREATE TABLE player (
     name varchar(50) NOT NULL
 );
 
--- Create the match table that contains all dats related to matches
+-- Create the match table that contains all data related to matches
 -- including which player won and which player lost.
 CREATE TABLE match(
     id serial PRIMARY KEY,
-    winnerId integer references player(id),
-    loserId integer references player(id)
+    winnerId integer references player(id) ON DELETE CASCADE,
+    loserId integer references player(id) ON DELETE CASCADE
 );
+
+ALTER table match
+    ADD CHECK(winnerId <> loserId);
+
 
 -- Create a standings views that displays the player standings including
 -- how many matches the players have played and how many of the mathes
@@ -30,5 +40,6 @@ SELECT mainPlayer.id, mainPlayer.name,
         WHERE mainPlayer.id = match.winnerId),
     (SELECT count(*) AS matches FROM match
         WHERE mainPlayer.id = match.winnerId OR mainPlayer.id = loserId)
-FROM player AS mainPlayer;
+FROM player AS mainPlayer
+ORDER BY wins;
 \q
